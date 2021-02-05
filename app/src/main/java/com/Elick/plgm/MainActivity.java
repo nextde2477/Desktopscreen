730539b809c07822713ee2200fc5ac697a83934c
nextde2477 <1547598968@qq.com>
2021-02-06 06:23:38 +0800

    简单的安卓小案例桌面屏幕

--- app/src/main/java/com/Elick/plgm/MainActivity.java
+++ app/src/main/java/com/Elick/plgm/MainActivity.java
@@ -2,13 +2,30 @@ package com.Elick.plgm;
 
 import android.app.*;
 import android.os.*;
+import java.util.Date;
+import java.util.Calendar;
+import android.widget.TextView;
+import android.view.Window;
+import android.view.WindowManager;
+import android.util.Log;
+import android.icu.text.SimpleDateFormat;
+import java.util.Locale;
 
 public class MainActivity extends Activity 
 {
+    long timecurrentTimeMillis = System.currentTimeMillis();
+    long timeGetTime = new Date().getTime();
+    long timeSeconds = System.currentTimeMillis();
+    long timeMillis = Calendar.getInstance().getTimeInMillis();
+    private TextView textview;
+
+    SimpleDateFormat realTime = new SimpleDateFormat("HH:mm:ss", Locale.getDefault());
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-    }
+        }
 }
